b1d2e602400cfd1c6d4a3228a470b249a2775760
BlagojeV93 <blagoje.vukovic@hotmail.com>
2018-01-30 14:30:17 +0100

    083

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   numbers = [1, 2, 3, 4, 5];
-  odd = [1, 3, 5];
-  even = [2, 4];
+  odd = [1, 3, 5, 6];
+  even = [2, 4, 7];
   onlyOdd = false;
 }
